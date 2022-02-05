b08edb43c72515069f1719992bfc7a7dc8e739f6
Kai <pitucktt@gmail.com>
2022-02-05 14:45:04 +0700

    Chnage a text in server.js

--- server.js
+++ server.js
@@ -10,4 +10,4 @@ const hospitals = require('./routes/hospitals');
 app.use('/api/v1/hospitals',hospitals);
 
 const PORT=process.env.PORT || 5000;
-app.listen(PORT,console.log("Server running in ", process.env.NODE_ENV, 'mode on port', PORT));
+app.listen(PORT,console.log("REST Server running in ", process.env.NODE_ENV, 'mode on port', PORT));
